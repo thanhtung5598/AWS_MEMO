6e66a6261f0fc74a46a6eda68e937afff74b8bf4
LeThanhTung <thanhtung5598@gmail.com>
2020-11-07 11:57:04 +0700

    first commit

--- daos/students.dao.js
+++ daos/students.dao.js
@@ -1,6 +1,11 @@
 const AWS = require("aws-sdk");
+const BUCKET_NAME = 'thanhtung';
 AWS.config.update({
   region: "ap-southeast-1",
+  accessKeyId: process.env.ACCESS_KEY_ID,
+  secretAccessKey: process.env.SECRET_ACCESS_KEY,
+});
+const S3 = new AWS.S3({
   accessKeyId: process.env.ACCESS_KEY_ID,
   secretAccessKey: process.env.SECRET_ACCESS_KEY,
 });
@@ -8,21 +13,31 @@ AWS.config.update({
 const docClient = new AWS.DynamoDB.DocumentClient();
 const table = "students";
 
-module.exports.getAllStudents = async () => {
+module.exports.getAll = async () => {
   const params = {
     TableName: table,
   };
   return await (await docClient.scan(params).promise()).Items;
 };
 
-module.exports.addStudent = async (data) => {
+module.exports.uploadAvatar = async (avatar) => {
+  const params = {
+    Bucket: BUCKET_NAME,
+    Key: avatar.name, // File name you want to save as in S3
+    Body: avatar.data,
+    ACL: "public-read",
+  };
+  return await (await S3.upload(params).promise()).Location;
+};
+
+module.exports.add = async (data) => {
   const params = {
     TableName: table,
     Item: data,
   };
   return await docClient.put(params).promise();
 };
-module.exports.deleteStudent = async (student_id) => {
+module.exports.delete = async (student_id) => {
   const params = {
     TableName: table,
     Key: {
--- routes/index.js
+++ routes/index.js
@@ -5,7 +5,36 @@ const studentDAO = require("../daos/students.dao");
 
 /* GET home page. */
 router.get("/", async (req, res) => {
-  const students = await studentDAO.getAllStudents();
+  const students = await studentDAO.getAll();
   res.render("index", { students });
 });
+
+router.post("/students/add", async (req, res) => {
+  let files = req.files;
+  let avatar = await files.avatar;
+  const uploadS3 = await studentDAO.uploadAvatar(avatar);
+  const student = {
+    student_id: req.body.studentId,
+    name: req.body.name,
+    birthdate: req.body.birthdate,
+    avatar: uploadS3,
+  };
+
+  const success = await studentDAO.add(student);
+  if (success) {
+    res.redirect("/");
+  } else {
+    res.status(400).send("Invalid");
+  }
+});
+
+router.get("/students/delete/:id", async (req, res) => {
+  const student_id = req.params.id;
+  const success = await studentDAO.delete(student_id);
+  if (success) {
+    res.redirect("/");
+  }
+  if (!success) res.status(400).send("invalid");
+});
+
 module.exports = router;
